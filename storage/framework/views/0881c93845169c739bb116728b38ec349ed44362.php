c1b1951b2166de13ef68ccccf6a5822ec2122311
Apple <apple@Apples-MacBook-Pro.local>
2018-11-28 16:55:06 +0530

    First commit

---
+++ storage/framework/views/0881c93845169c739bb116728b38ec349ed44362.php
@@ -0,0 +1,8 @@
+<?php echo Form::open(array('route' => 'clear-activity')); ?>
+
+    <?php echo Form::hidden('_method', 'DELETE'); ?>
+
+    <?php echo Form::button('<i class="fa fa-fw fa-trash" aria-hidden="true"></i>' . trans('LaravelLogger::laravel-logger.dashboard.menu.clear'), array('type' => 'button', 'data-toggle' => 'modal', 'data-target' => '#confirmDelete', 'data-title' => trans('LaravelLogger::laravel-logger.modals.clearLog.title'),'data-message' => trans('LaravelLogger::laravel-logger.modals.clearLog.message'), 'class' => 'dropdown-item')); ?>
+
+<?php echo Form::close(); ?>
+
